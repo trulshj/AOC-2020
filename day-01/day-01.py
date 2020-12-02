f3744729b596128cd515d55094f62cac06b95b58
Truls Henrik Jakobsen <trulshj@stud.ntnu.no>
2020-12-02 13:08:58 +0100

    update day 1

--- day-01/day-01.py
+++ day-01/day-01.py
@@ -2,7 +2,7 @@ import time
 
 start_time = time.time()
 
-expense_report = [int(x.rstrip()) for x in open("day-01/input-01.txt").readlines()]
+expense_report = [int(x.rstrip()) for x in open("input-01.txt").readlines()]
 
 print()
 print(" Day 1 ".center(64, '-'))
@@ -16,7 +16,8 @@ for idx1, val1 in enumerate(expense_report):
     for idx2, val2 in enumerate(expense_report[idx1+1:]):
         for val3 in expense_report[idx2+idx1+1:]:
             if val1 + val2 + val3 == 2020:
-                print(f"Part 2: {val1} * {val2} * {val3} = {val1 * val2 * val3}".center(64))
+                print(
+                    f"Part 2: {val1} * {val2} * {val3} = {val1 * val2 * val3}".center(64))
 
 print("\n")
 print(f"Found in {time.time() - start_time} seconds".center(64))
